54f85033a8f4168f647d1b2e70f815731686926c
jdsmith104 <jdsmith104@outlook.com>
2024-03-28 15:16:59 +0000

    auth before each transaction

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from requests import HTTPError
 import puregym
 import json
 import logging
@@ -6,58 +7,85 @@ import os
 import sys
 import datetime
 
-TARGET_GYM_IDS = [339, 286, 296, 168, 120, 222, 129]
+TARGET_GYM_IDS = [339, 286, 296, 168, 120, 222, 129, 49]
 POLL_INTERVAL = 270
-HEADER="datetime,"+",".join(map(str, TARGET_GYM_IDS))
+HEADER = "datetime,"+",".join(map(str, TARGET_GYM_IDS))
 LOG_LEVEL = logging.INFO
 LOG_DIR = "logs"
 
-def main(email: str, pin: str, logger: logging.Logger):
-    
-    client = puregym.PuregymAPIClient()
+
+def setup_logging() -> logging.Logger:
+    # Create directory for logs if it does not exist
+    logfile_path = LOG_DIR+"/log_"+time.strftime("%Y%m%d-%H%M%S")
+    os.makedirs(os.path.dirname(logfile_path), exist_ok=True)
+
+    logging.basicConfig(filename=logfile_path,
+                        filemode='a',
+                        format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=LOG_LEVEL)
+
+    return logging.getLogger('PUREGYM_ATTENDANCE_SCRAPER')
+
+
+def login(client, email, pin):
     client.login(email, pin)
 
+
+def get_attendance(client) -> list[str]:
+    return [str(client.get_gym_attendance(gym_id)) for gym_id in TARGET_GYM_IDS]
+
+
+def log(logger, attendance):
+    logline = datetime.datetime.now().isoformat()+","+",".join(attendance)
+    logger.info(logline)
+
+
+def run(email: str, pin: str, logger: logging.Logger):
     run = True
-
+    # setup client
+    client = puregym.PuregymAPIClient()
     logger.info(HEADER)
-
+    # Loop
     while run:
         try:
-            all_attendance = [str(client.get_gym_attendance(gym_id)) for gym_id in TARGET_GYM_IDS]
-            logline = datetime.datetime.now().isoformat()+","+",".join(all_attendance)
-            logger.info(logline)
+            # auth
+            login(client, email, pin)
 
+            if client.authed:
+                # get data
+                all_attendance = get_attendance(client)
+
+                # log
+                log(logger, all_attendance)
+            else:
+                logger.error("\nClient not authed")
+
+            # wait
             time.sleep(POLL_INTERVAL)
+        except HTTPError as e:
+            logger.error(f"{e} and response={e.response}")
+            time.sleep(POLL_INTERVAL*2)
         except KeyboardInterrupt:
-            print('\nStopping server')
+            logger.info('\nStopping server')
+            run = False
             try:
                 sys.exit(130)
             except SystemExit:
                 os._exit(130)
 
-def setup_logging() -> logging.Logger:
-    # Create directory for logs if it does not exist 
-    logfile_path = LOG_DIR+"/log_"+time.strftime("%Y%m%d-%H%M%S")
-    os.makedirs(os.path.dirname(logfile_path), exist_ok=True)
-    
-    logging.basicConfig(filename=logfile_path,
-                    filemode='a',
-                    format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=LOG_LEVEL)
-
-    return logging.getLogger('PUREGYM_ATTENDANCE_SCRAPER')
 
 if __name__ == "__main__":
     print("running server")
 
-    logger = setup_logging()
-
+    # Get credentials from env
     with open("credentials.json", "r") as f:
         credentials = json.load(f)
-    
+
     email = credentials.get("email")
     pin = credentials.get("pin")
+
+    logger = setup_logging()
     logger.debug(f"Credentials email={email} pin={pin}")
 
-    main(email, pin, logger)
+    run(email, pin, logger)
